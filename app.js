b015d69ced7fd5b6b2dd34e63223156ed6a44412
HANARAE <1532163@naver.com>
2023-04-14 18:25:42 +0900

    20230414(금)

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
+const grill = {
+    '1': false,
+    '2': false,
+    '3': false,
+    '4': false,
+    '5': false,
+    '6': false,
+    '7': false,
+    '8': false
+}
+
 const $btn = document.querySelector('.start-btn');
 const $seconds = document.querySelector('.seconds');
-
 let sec = 60;
 $seconds.textContent = sec;
+const $box = document.querySelectorAll('.box > img');
+
+//고기생성함수
+function randomMeat(e) {
+    let meatNum = Math.floor(Math.random() * 8 + 1);
+    if (!grill[meatNum]) {
+        grill[meatNum] = true;
+        $box[meatNum-1].classList.toggle('show');
+    } else {
+        meatNum = Math.floor(Math.random() * 8 + 1);
+    }
+}
+
+//고기익기함수
+
+//고기태우기함수
+
 
 $btn.onclick = function (e) {
     e.target.parentNode.classList.toggle('hide');
@@ -10,18 +37,41 @@ $btn.onclick = function (e) {
         e.target.parentNode.style.display = 'none';
     }, 1001);
 
-    function TIMER(){
-        PlAYTIME=setInterval(function(){
-           if(sec>0){
-                sec=sec-1;
+
+    //시작버튼 누르면 
+    function TIMER() {
+
+        //랜덤하게 고기 생성
+        const func2 = setInterval(function () {
+            randomMeat();
+        }, Math.floor(Math.random() * 2 + 1) * 1000);
+
+        //남은시간 60초에서 0초까지 줄어듦
+        const func = setInterval(function () {
+            if (sec > 0) {
+                sec = sec - 1;
                 $seconds.textContent = sec;
             }
-            if(sec===0){
-                $seconds.textContent='0'
-                
-            }    
-        },1000);
+            if (sec === 0) {
+                $seconds.textContent = '0'
+            }
+        }, 1000);
+
+
+
+
+
+
     }
     TIMER();
+
+
+
 }
 
+
+
+// const func = setInterval(function() {
+
+// }
+// }, 300);
